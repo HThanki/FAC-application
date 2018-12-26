01d6e3e3ff22e296b715a382b1f8896bae3925ba
Harsheek Thanki <harsheekthanki@gmail.com>
2018-12-26 00:36:02 +0000

    Collapseable 'Read more' button functional for all 3 past, present and future cards

--- myscript.js
+++ myscript.js
@@ -1,14 +1,42 @@
 let i=0;
-function read(){
+function pastRead(){
   if(!i) {
-    document.getElementById("more").style.display = "inline";
-    document.getElementById("dots").style.display = "none";
-    document.getElementById("read").innerHTML="Read less";
+    document.getElementById("pastMore").style.display = "inline";
+    document.getElementById("pastDots").style.display = "none";
+    document.getElementById("pastRead").innerHTML="Read less";
     i=1;
     } else {
-    document.getElementById("more").style.display = "none";
-    document.getElementById("dots").style.display = "inline";
-    document.getElementById("read").innerHTML="Read more";
+    document.getElementById("pastMore").style.display = "none";
+    document.getElementById("pastDots").style.display = "inline";
+    document.getElementById("pastRead").innerHTML="Read more";
+    i=0;
+  }
+}
+
+function presentRead(){
+  if(!i) {
+    document.getElementById("presentMore").style.display = "inline";
+    document.getElementById("presentDots").style.display = "none";
+    document.getElementById("presentRead").innerHTML="Read less";
+    i=1;
+    } else {
+    document.getElementById("presentMore").style.display = "none";
+    document.getElementById("presentDots").style.display = "inline";
+    document.getElementById("presentRead").innerHTML="Read more";
+    i=0;
+  }
+}
+
+function futureRead(){
+  if(!i) {
+    document.getElementById("futureMore").style.display = "inline";
+    document.getElementById("futureDots").style.display = "none";
+    document.getElementById("futureRead").innerHTML="Read less";
+    i=1;
+    } else {
+    document.getElementById("futureMore").style.display = "none";
+    document.getElementById("futureDots").style.display = "inline";
+    document.getElementById("futureRead").innerHTML="Read more";
     i=0;
   }
 }
